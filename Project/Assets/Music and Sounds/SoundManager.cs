3196477f7deb4249b2e087975b50a6198d8f302a
FreeOnur <144993689+FreeOnur@users.noreply.github.com>
2025-06-16 23:39:23 +0200

    Added SoundManager script

---
+++ Project/Assets/Music and Sounds/SoundManager.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    [Header("Audio Sources")]
+    public AudioSource musicSource;
+    public AudioSource sfxSource;
+
+    [Header("Audio Clips")]
+    public List<NamedClip> musicClips;
+    public List<NamedClip> sfxClips;
+
+    private Dictionary<string, AudioClip> musicDict;
+    private Dictionary<string, AudioClip> sfxDict;
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        musicDict = LoadClipDictionary(musicClips);
+        sfxDict = LoadClipDictionary(sfxClips);
+    }
+
+    private Dictionary<string, AudioClip> LoadClipDictionary(List<NamedClip> clips)
+    {
+        var dict = new Dictionary<string, AudioClip>();
+        foreach (var clip in clips)
+            if (!dict.ContainsKey(clip.name))
+                dict.Add(clip.name, clip.clip);
+        return dict;
+    }
+    // for background music etc
+    public void PlayMusic(string name, bool loop = true)
+    {
+        if (musicDict.TryGetValue(name, out var clip))
+        {
+            musicSource.clip = clip;
+            musicSource.loop = loop;
+            musicSource.Play();
+        }
+    }
+
+    public void StopMusic()
+    {
+        musicSource.Stop();
+    }
+
+    public void PauseMusic()
+    {
+        musicSource.Pause();
+    }
+    public void ResumeMusic()
+    {
+        musicSource.UnPause();
+    }
+    // more like for effects
+    public void PlaySFX(string name)
+    {
+        if (sfxDict.TryGetValue(name, out var clip))
+            sfxSource.PlayOneShot(clip);
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        AudioListener.volume = volume;
+    }
+    public void SetMusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+    }
+    public void SetSFXVolume(float volume)
+    {
+        sfxSource.volume = volume;
+    }
+
+    public void MuteAll(bool mute)
+    {
+        AudioListener.pause = mute;
+    }
+}
+
+[System.Serializable]
+public class NamedClip
+{
+    public string name;
+    public AudioClip clip;
+}
